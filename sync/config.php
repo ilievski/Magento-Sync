19435d8f36182a1d95016883ffaf8973c3319b41
igor <ilievski156@gmail.com>
2016-05-25 15:03:46 +0200

    Update config.php

--- sync/config.php
+++ sync/config.php
@@ -2,9 +2,9 @@
 	/* 
 	 * LIVE SITE CONFIG 
 	 */
-	$s1_db_name = 'coffee_a'; // Database Name
-	$s1_db_user = 'root';  // Database User
-	$s1_db_pass = 'root'; // Database Password
+	$s1_db_name = 'live_site_db'; // Database Name
+	$s1_db_user = 'db_user';  // Database User
+	$s1_db_pass = 'db_password'; // Database Password
 	$s1_root_dir = '/var/www/htdocs/example.com'; // Site Base Directory
 	$s1_base_url = 'http://www.example.com'; // Site Base Url
 	$s1_secure_url = 'http://www.example.com/'; // Change 'http' to 'https' if SSL enabled
@@ -12,9 +12,9 @@
 	/* 
 	 * STAGING SITE CONFIG 
 	 */
-	$s2_db_name = 'coffee_b'; // Database Name
-	$s2_db_user = 'root'; // Database User
-	$s2_db_pass = 'root'; // Database Password
+	$s2_db_name = 'staging_site_db'; // Database Name
+	$s2_db_user = 'db_user'; // Database User
+	$s2_db_pass = 'db_password'; // Database Password
 	$s2_root_dir = '/var/www/htdocs/staging.example.com'; // Site Base Directory
 	$s2_base_url = 'http://staging.example.com/'; // Site Base Url
 ?>
